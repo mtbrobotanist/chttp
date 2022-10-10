e79aa725205c28cc497ce3ac065c95a59fa70b6d
Nate <floydwin7@gmail.com>
2022-10-09 22:59:55 -0700

    add initial files

---
+++ src/server.c
@@ -0,0 +1,8 @@
+#include <http/http.h>
+
+#include <stdio.h>
+
+void http_serve()
+{
+    printf("http hello\n");
+}
---
+++ test/main.c
@@ -0,0 +1,6 @@
+#include <http/http.h>
+
+int main(int argc, char **argv)
+{
+    http_serve();
+}
